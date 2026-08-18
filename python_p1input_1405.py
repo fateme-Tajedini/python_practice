b8d06a17bfa63b2b7f1f2c347226791c15321387
fateme-Tajedini <f.tajedini83@gmail.com>
2026-08-18 16:57:29 +0330

    Add basic input and sum exercise.

---
+++ python_p1input_1405.py
@@ -0,0 +1,11 @@
+# A program that takes three numbers as input and displays their sum.
+
+number1 = int(input("Enter a number:"))
+number2 = int(input("Enter a number:"))
+number3 = int(input("Enter a number:"))
+
+sum = number1 + number2 + number3
+
+print(sum)
+
+# complete
