85f6532663907dd27a5c2dea98a1eb46cb00a0d8
fateme-Tajedini <f.tajedini83@gmail.com>
2026-08-20 00:02:03 +0330

    Add   even and odd numbers_while loop

---
+++ python_p4while_1405.py
@@ -0,0 +1,24 @@
+# A program than prints even numbers from 2 to n.
+
+n = int(input("Enter N: "))
+
+i= 2
+
+while i<= n :
+    print(i)
+    i += 2
+
+
+ # complete
+
+
+
+# print Odd numbers from 1 to n.
+
+n = int(input("Enter n :"))
+i = 1
+while i<=n :
+    print(i)
+    i += 2
+
+# complete
